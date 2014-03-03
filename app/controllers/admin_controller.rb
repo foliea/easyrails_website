0df13be6b03892c7ab6d00acf91e9810966b995b
Adrien Folie <folie.adrien@gmail.com>
2014-03-02 20:01:46 -0500

    clean

--- app/controllers/admin_controller.rb
+++
@@ -1,3 +0,0 @@
-class Admins::SessionsController < Devise::SessionsController
-  before_filter :authenticate_admin!, only: [:show, :new, :create, :edit, :update, :destroy]
-end
--- config/initializers/session_store.rb
+++
@@ -1,3 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-FolieMyblog::Application.config.session_store :cookie_store, key: '_folie_myblog_session'
