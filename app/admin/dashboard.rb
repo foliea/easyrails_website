d2af8c3d8e4b557dee96cd0392989be1346befdd
Adrien Folie <folie.adrien@gmail.com>
2014-03-05 09:50:39 -0500

    integration

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,13 +3,16 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+    section "Recent Users" do
+      table_for User.order("created_at desc").limit(5) do
+        column :email do |user|
+          link_to user.email, admin_user_path(user)
+        end
+        column :created_at
       end
+      strong { link_to "View All Users", admin_users_path }
     end
-
-
   end
+  
+ 
 end
