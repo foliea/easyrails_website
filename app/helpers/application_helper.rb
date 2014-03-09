99b8a686c82c9db25c0177fb75b377d970c1735a
Adrien Folie <folie.adrien@gmail.com>
2014-03-09 17:27:45 +0100

    navigation changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,20 @@
 module ApplicationHelper
   def title(page_title)
      content_for(:title) { page_title }   
-  end
-  
-  def site_title
-    return AppConfig['site_title']
-  end
+  end 
   
   def site_owner
     return AppConfig['site_owner']
   end
-  
+
+  def site_title
+    return AppConfig['site_title']
+  end
+
+  def site_title_link
+    return AppConfig['site_title_link']
+  end
+
   def languages_available
     @languages = Language.all
     return @languages
