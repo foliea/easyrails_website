c37337740379663d3db9539518b3a91232d1002f
Adrien Folie <folie.adrien@gmail.com>
2014-03-19 23:46:10 +0100

    first unit test

--- test/models/language_test.rb
+++ test/models/language_test.rb
@@ -1,7 +1,36 @@
 require 'test_helper'
 
 class LanguageTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "invalid without a name" do
+    l = languages(:fr)
+    l.name = nil
+    assert !l.valid?, 'Name is not being validated'
+  end
+
+  test "invalid without a code" do
+    l = languages(:fr)
+    l.code = nil
+    assert !l.valid?, 'Code is not being validated'
+  end
+
+  test "invalid if code already exist" do
+    l_fr = languages(:fr)
+    l_en = languages(:en)
+    l_en.code = l_fr.code
+
+    assert !l_en.valid?, 'Uniqueness of Code is not being validated'
+  end
+
+  test "valid without default" do
+    l = languages(:fr)
+    l.default = nil
+
+    assert l.valid?, 'Language wasn\'t valid without default'
+  end
+
+  test "valid with all attributes" do
+    l = languages(:fr)
+
+    assert l.valid?, 'Language wasn\'t valid'
+  end
 end
