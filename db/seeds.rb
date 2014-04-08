00b8fa9184b4088f75037653013e128b2f45a8da
Adrien Folie <folie.adrien@gmail.com>
2014-04-08 07:06:16 +0200

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,10 @@ Language.create(name: 'English', code: 'en', default: true)
 
 settings = [
   # Default settings
-  { keyname: 'site_title',            value: 'EasyRails Website',          value_format: 'string' },
-  { keyname: 'site_title_link',       value: '127.0.0.1:3000',             value_format: 'string' },
-  { keyname: 'site_owner',            value: 'EasyRails',                  value_format: 'string' },
-  { keyname: 'site_description',      value: 'This is easy rails website', value_format: 'string' },
+  { keyname: 'site_title',            value: 'Adrien Folie',               value_format: 'string' },
+  { keyname: 'site_title_link',       value: 'www.foliea.io',              value_format: 'string' },
+  { keyname: 'site_owner',            value: 'Adrien Folie',               value_format: 'string' },
+  { keyname: 'site_description',      value: 'Adrien Folie blog',          value_format: 'string' },
   { keyname: 'site_date_start_end',   value: '2014',                       value_format: 'string' },
   # Amazon Web Services
   { keyname: 's3_host_name',          value: 's3-eu-west-1.amazonaws.com', value_format: 'string' }
