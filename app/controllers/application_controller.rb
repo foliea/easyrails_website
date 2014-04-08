9893805cd419af2f5014f48f7fcf573883073059
Adrien Folie <folie.adrien@gmail.com>
2014-04-08 07:20:22 +0200

    fix bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  #before_action :set_user_params
+  before_action :set_default_locale
   before_action :set_locale
   before_action :set_languages_available
 
