59c2b4bc0048c9813d5f10b6a71cf48b8cd07a58
Adrien Folie <folie.adrien@gmail.com>
2014-04-10 07:43:05 +0200

    remove static assets

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module FolieMyblog
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
     #config.assets.initialize_on_precompile = false
-    config.serve_static_assets = true
+    #config.serve_static_assets = true
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
