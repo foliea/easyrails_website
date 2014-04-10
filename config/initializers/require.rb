732f34f017e2a80d79e8d2936a80abdecc8a04bd
Adrien Folie <folie.adrien@gmail.com>
2014-04-10 07:54:48 +0200

    special model => lib

--- config/initializers/require.rb
+++ config/initializers/require.rb
@@ -1,1 +1,2 @@
-Dir[File.join(Rails.root, "app", "others", "**", "*.rb")].each { |path| require path }
+Dir[File.join(Rails.root, 'app', 'others', '**', '*.rb')].each { |path| require path }
+Dir[File.join(Rails.root, 'lib', 'special_model', '**', '*.rb')].each { |path| require path }
