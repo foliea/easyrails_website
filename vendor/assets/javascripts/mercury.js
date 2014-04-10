19fbb490e85df9b93d897f9b5bfed7477056a2f4
Adrien Folie <folie.adrien@gmail.com>
2014-04-10 07:07:37 +0200

    deploy

--- vendor/assets/javascripts/mercury.js
+++ vendor/assets/javascripts/mercury.js
@@ -2,8 +2,7 @@
  * Mercury Editor is a CoffeeScript and jQuery based WYSIWYG editor.  Documentation and other useful information can be
  * found at https://github.com/jejacks0n/mercury
  *
- *= require jquery
- *= require jquery-migrate-min
+ * = require jquery-1.7
  * Minimum jQuery requirements are 1.7
  *= require_self
  *
