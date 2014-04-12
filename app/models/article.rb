bc86eeb84b6ce363a316834b0861080cd28b604c
Adrien Folie <folie.adrien@gmail.com>
2014-04-12 19:14:49 +0200

    removed articles

--- app/models/article.rb
+++
@@ -1,7 +0,0 @@
-class Article < ActiveRecord::Base
-  include Commentable
-
-  belongs_to :user
-
-  validates :locale, presence: true
-end
--- app/models/setting.rb
+++ app/models/setting.rb
@@ -4,8 +4,6 @@ class Setting < ActiveRecord::Base
   validates :keyname, :value, presence: true
   validates :keyname, uniqueness: true
 
-  #silent_fail_check :validation, :keyname, :value
-
   protected
 
   def reload
