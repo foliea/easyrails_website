a751f316821573576ac2cdc66cd809da6d89c4bd
Adrien Folie <folie.adrien@gmail.com>
2014-04-12 19:22:28 +0200

    drop table articles

--- db/schema.rb
+++ db/schema.rb
@@ -11,19 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140412144144) do
+ActiveRecord::Schema.define(version: 20140412172133) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "articles", force: true do |t|
-    t.string   "title"
-    t.text     "body"
-    t.string   "locale"
-    t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
   create_table "comments", force: true do |t|
     t.integer  "user_id"
