390d559089f2e9b57ca376b410e1929714bc2edc
Adrien Folie <folie.adrien@gmail.com>
2014-04-18 11:44:41 +0200

    Update settings.rb

--- app/others/settings.rb
+++ app/others/settings.rb
@@ -1,12 +1,12 @@
-class Settings
-  def self.reload
+module Settings
+  extend self
+  
+  def reload
     AppConfig.reload
     ActiveAdmin.setup do |c|
-      c.site_title = AppConfig['site_title']
-      c.site_title_link = AppConfig['site_title_link']
+      c.site_title = proc { AppConfig['site_title'] }
+      c.site_title_link = proc { AppConfig['site_title_link'] }
     end
-    if AppConfig['s3_host_name'].present?
-      Paperclip::Attachment.default_options[:s3_host_name] = AppConfig['s3_host_name']
-    end
+    Paperclip::Attachment.default_options[:s3_host_name] = proc { AppConfig['s3_host_name'] }
   end
 end
