547649fdfe385f5af98da5e8c0b1a47936bb0a6e
Adrien Folie <folie.adrien@gmail.com>
2014-04-19 21:39:04 +0200

    comment : remove

--- app/models/user_comment.rb
+++
@@ -1,7 +0,0 @@
-class UserComment < SimpleDelegator
-  def create item, message
-    comment = self.comments.new(commentable: item, message: message)
-    comment.save
-    comment
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,22 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140419184512) do
+ActiveRecord::Schema.define(version: 20140419193808) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "comments", force: true do |t|
-    t.integer  "user_id"
-    t.string   "message"
-    t.integer  "commentable_id"
-    t.string   "commentable_type"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "comments", ["commentable_id", "commentable_type"], name: "index_comments_on_commentable_id_and_commentable_type", using: :btree
-  add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
 
   create_table "documents", force: true do |t|
     t.string   "document_file_name"
