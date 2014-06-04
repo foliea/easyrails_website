4089a6bd3f626102e2094104740597b7dff3839a
Adrien Folie <folie.adrien@gmail.com>
2014-06-04 15:00:27 +0200

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,20 +4,25 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :set_locale
-  before_action :set_languages_available
+  before_cation :set_available_locales
+  before_action :set_available_languages
 
   protected
 
   def set_locale
     # update sessions if passed
-    if params[:locale].present? && I18n.available_locales.include?(params[:locale].to_sym)
+    if params[:locale].present? && @available_locales
       session[:locale] = params[:locale]
     end
     # set locale based on sessions
     I18n.locale = session[:locale] ||= I18n.default_locale
   end
 
-  def set_languages_available
-    @languages_available ||= Language.all
+  def set_available_locales
+    @available_locales ||= Language.available_locales
+  end
+
+  def set_available_languages
+    @available_languages ||= Language.all
   end
 end
