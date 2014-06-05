96556ef34f208c8b353af34138b5235fb0de0e80
Adrien Folie <folie.adrien@gmail.com>
2014-06-05 16:09:18 +0200

    Update image_spec.rb

--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -1,4 +1,11 @@
 require 'spec_helper'
 
 describe Image do
+  it { should have_attached_file(:image) }
+  it { should validate_attachment_presence(:image) }
+  it { should validate_attachment_content_type(:image).
+                allowing('image/png', 'image/gif', 'image.jpg').
+                rejecting('text/plain', 'text/xml') }
+  it { should validate_attachment_size(:image).
+                less_than(5.megabytes) }
 end
