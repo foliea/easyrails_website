912da1f07676704f3cf1c3812bc04bf137fb8809
Adrien Folie <folie.adrien@gmail.com>
2014-06-05 15:52:58 +0200

    Update page_spec.rb

--- spec/models/page_spec.rb
+++ spec/models/page_spec.rb
@@ -1,5 +1,31 @@
 require 'spec_helper'
 
 describe Page do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:locale) }
+  it { should validate_uniqueness_of(:name).scoped_to(:locale) }
+  
+  context 'when saving' do
+    it 'parameterize the name' do
+      
+    end
+  end
+  
+  context 'when current locale exist' do
+    it 'gets language by name from current locale'
+    
+    end
+  end
+  
+  context "when current locale doesn't exist" do
+    it 'gets language by name from default locale'
+    
+    end
+  end
+  
+  context "when default locale doesn't exist" do
+    it 'gets first existant language with name'
+    
+    end
+  end
 end
