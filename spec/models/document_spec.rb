34cb12c410a4fd79881b0243bf9024264588e773
Adrien Folie <folie.adrien@gmail.com>
2014-06-05 16:10:30 +0200

    Update document_spec.rb

--- spec/models/document_spec.rb
+++ spec/models/document_spec.rb
@@ -1,4 +1,11 @@
 require 'spec_helper'
 
 describe Document do
+  it { should have_attached_file(:document) }
+  it { should validate_attachment_presence(:document) }
+  it { should validate_attachment_content_type(:document).
+                allowing('application/pdf', 'application/msword').
+                rejecting('text/plain', 'text/xml') }
+  it { should validate_attachment_size(:document).
+                less_than(20.megabytes) }
 end
