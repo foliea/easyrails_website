7aa817080ec65d5ece13d56d88ed8a521351888d
Adrien Folie <folie.adrien@gmail.com>
2014-06-05 16:03:36 +0200

    Update user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,36 @@
 include 'spec_helper'
 
 describe User do
+  it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:password) }
   
+  context 'when already registered from a provider'
+    it 'gets the user' do
+      
+    end
+  end
+  
+  context 'when email from provider already exist'
+    it 'gets the user' do
+      
+    end
+  end
+  
+  context 'when registering from external provider'
+    it 'creates a new user' do
+      
+    end
+  end
+  
+  context 'before create'
+    it 'creates a profile' do
+      
+    end
+  end
+  
+  context 'when destroying'
+    it 'destroy affiliated profile' do
+      
+    end
+  end
 end
