3f0f86fe8fe85b23bef76646b60f4fdcd01cc380
Adrien Folie <folie.adrien@gmail.com>
2014-06-17 02:28:12 +0200

    improve integration tests

--- spec/features/account_registration_spec.rb
+++ spec/features/account_registration_spec.rb
@@ -3,13 +3,24 @@ require 'spec_helper'
 feature 'Account registration' do
   include Support::Authentication
 
-  scenario 'succeed when user is valid' do
-    register(:user)
-    expect(current_path).to eq(root_path)
+  context 'when user is valid' do
+    scenario 'redirects to root' do
+      register(:user)
+      expect(current_path).to eq(root_path)
+    end
+
+    scenario 'displays success notice' do
+      register(:user)
+      msg = I18n.t('devise.registrations.signed_up')
+
+      expect(page.has_content?(msg)).to be true
+    end
   end
 
-  scenario 'failed when user is invalid' do
-    register(:user_error)
-    expect(current_path).to eq(user_registration_path)
+  context 'when user is invalid' do
+    scenario 'redirects to user registration' do
+      register(:user_error)
+      expect(current_path).to eq(user_registration_path)
+    end
   end
 end
--- spec/features/account_sign_in_spec.rb
+++ spec/features/account_sign_in_spec.rb
@@ -5,13 +5,21 @@ feature 'Account sign in' do
 
   let!(:user) { FactoryGirl.create(:user) }
 
-  scenario 'succeed when user exist' do
-    sign_in(:user)
-    expect(current_path).to eq(root_path)
+  context 'when user exist' do
+    scenario 'displays success notice' do
+      sign_in(:user)
+      msg = I18n.t('devise.sessions.signed_in')
+
+      expect(page.has_content?(msg)).to be true
+    end
   end
 
-  scenario "failed when user doesn't exist" do
-    sign_in(:user_twitter)
-    expect(current_path).to eq(root_path)
+  context "when user doesn't exist" do
+    scenario 'displays invalid alert' do
+      sign_in(:user_error)
+      msg = I18n.t('devise.failure.invalid')
+
+      expect(page.has_content?(msg)).to be true
+    end
   end
 end
