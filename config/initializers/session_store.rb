db3a29e666286367803f54b0e5e0948c3c228b62
Adrien Folie <folie.adrien@gmail.com>
2014-06-22 18:18:22 +0200

    move cookie key as env variable
    Fix issue #12

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,2 +1,2 @@
 # Be sure to restart your server when you modify this file.
-EasyRailsWebsite::Application.config.session_store :cookie_store, key: '_easyrails_website_session'
+EasyRailsWebsite::Application.config.session_store :cookie_store, key: ENV['COOKIE_SESSION_KEY']
